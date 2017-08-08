19ef1afe8ab18c3e93e457e2c971b0d5a8250665
odilon <orafanambinantsoa@bocasay.com>
2017-08-08 15:07:10 +0200

    fist

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,AlertController  } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
+  		console.log('constructeur');
   }
-
+  alertAction():void{
+  	console.log('bouton');
+  	let alert = this.alertCtrl.create({
+      title: 'New Friend!',
+      subTitle: 'Your friend, Obi wan Kenobi, just accepted your friend request!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
 }
